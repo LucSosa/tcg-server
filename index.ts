af0ef759113fef1f5c08af0760624a42e7feb703
LucSosa <l.sosamachado@gmail.com>
2024-08-09 19:44:30 -0300

    Feat: Frontend Integration

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { UserController } from "./controllers/userController";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+})
+
+app.use(bodyParser.json())
 
 const userController = new UserController(app);
 userController.listenMethods();
